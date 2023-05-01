bef4be361743f40061da03ff0f226925b721cf93
Israel Herrera <113934681+OldSchoolIzzy@users.noreply.github.com>
2023-05-01 12:41:56 -0500

    Code cleanup
    Added form validation for creating tickets, added image upload to creating a ticket and viewing a ticket with no image. Small code clean, too.

--- RAP/urls.py
+++ RAP/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
+    path('triage/', include('django.contrib.auth.urls')),
     path('triage/', include('TicketSystem.urls')),
     path('system/', include('ReportingSystem.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
